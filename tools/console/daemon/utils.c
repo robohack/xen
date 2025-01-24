61e8a5c67d5ce275aec250e90754e0a0f1372930
Greg A. Woods <woods@robohack.ca>
2025-01-24 10:50:18 -0800

    tools/console: fix unportable use of '%m' with the dolog() macro
    The dolog() macro currently, conditionally, calls fprintf() as well as
    syslog(), but since '%m' is only usable with syslog() on some systems,
    avoid its use and use '%s' with strerror() instead.
    
    Signed-off-by: Greg A. Woods <woods@robohack.ca>

--- tools/console/daemon/utils.c
+++ tools/console/daemon/utils.c
@@ -107,13 +107,15 @@ bool xen_setup(void)
 	xs = xs_open(0);
 	if (xs == NULL) {
 		dolog(LOG_ERR,
-		      "Failed to contact xenstore (%m).  Is it running?");
+		      "Failed to contact xenstore (%s).  Is it running?",
+		      strerror(errno));
 		goto out;
 	}
 
 	xc = xc_interface_open(0,0,0);
 	if (!xc) {
-		dolog(LOG_ERR, "Failed to contact hypervisor (%m)");
+		dolog(LOG_ERR, "Failed to contact hypervisor (%s)",
+		      strerror(errno));
 		goto out;
 	}
 
