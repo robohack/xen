be3900c670d695f16e2434e7a3fca5e1403dbcef
Greg A. Woods <woods@robohack.ca>
2025-01-29 13:07:37 -0800

    tools/ocaml/xenstored: trim whitespace from number strings
    From the original pkgsrc patch for Xen 4.1:
    
    - Trim whitespace off a numeric string read out of the kernel.
      Instead of open coding the function, use String.trim, as the
      String library is used, anyway. (available in ocaml >= 4.00.1)
    
    Signed-off-by: Greg A. Woods <woods@robohack.ca>

--- tools/ocaml/xenstored/utils.ml
+++ tools/ocaml/xenstored/utils.ml
@@ -86,7 +86,7 @@ let read_file_single_integer filename =
   let buf = Bytes.make 20 '\000' in
   let sz = Unix.read fd buf 0 20 in
   Unix.close fd;
-  int_of_string (Bytes.sub_string buf 0 sz)
+  int_of_string (String.trim (Bytes.sub_string buf 0 sz))
 
 (* @path may be guest data and needs its length validating.  @connection_path
  * is generated locally in xenstored and always of the form "/local/domain/$N/" *)
